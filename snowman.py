90dd5ca2637e0c189a91938bfba6a740023943bf
Walter Ellis <mail.walterellis@gmail.com>
2025-12-18 08:34:19 +0100

    Step 4: Added play again loop enhancement

--- snowman.py
+++ snowman.py
@@ -1,4 +1,13 @@
 from game_logic import play_game
 
 if __name__ == "__main__":
-    play_game()
+    while True:
+        play_game()
+
+        again = input("\nDo you want to play again? (y/n): ").lower()
+
+        if again != "y":
+            print("Thanks for playing! Goodbye!")
+            break
+
+        print("\n" + "=" * 30 + "\n")
